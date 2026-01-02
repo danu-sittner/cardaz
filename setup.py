aba77380317b41b4cbbfd617d026ba6cf7fd845d
bblabbla <danu.sittner@gmail.com>
2026-01-02 18:27:57 +0200

    Fix formatting and update author name in setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-from pathlib import path
+from pathlib import Path
 from setuptools import find_packages, setup
-from typing import list
+from typing import List
 
 
 REQUIREMENTS_FILE = Path('.') / 'requirements.txt'
 
+
 def _read_requirements(file: Path) -> List[str]:
-	with open(file) as req_file:
-		lines = req_file.readlines()
-	return [line.rstrip() for line in lines]
+    with open(file) as req_file:
+        lines = req_file.readlines()
+    return [line.rstrip() for line in lines]
 
 
 setup(
-	name='cardazim',
-	version='0.0.1',
-	author='cardazim',
-	author_email='cardazim@protonmail.com',
-	description='A template for the cardazim project!',
-	long_description='''A basic template intended as a reference guide for the Cardazim Project!,
-	Use wisely,
-	''',
-	packages='find_packages(cardazim)',
-	install_requirements=_read_requirements(REQUIREMENTS_FILE)
+    name='cardazim',
+    version='0.0.1',
+    author='Cardazim',
+    author_email='cardazim@protonmail.com',
+    description='A template for the cardazim project!',
+    long_description='''A basic template intended as a reference guide for the Cardazim Project!
+    Use wisely!
+    ''',
+    packages=find_packages('cardazim'),
+    install_requires=_read_requirements(REQUIREMENTS_FILE)
 )
