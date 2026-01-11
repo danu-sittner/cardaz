d794ea727f5b494c4aebb00ebd6175427a79c818
Daniel <>
2026-01-11 18:45:45 +0200

    no more ugly

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 import argparse
 import sys
-import socket
-import struct
+from connection import Connection
 
 FAILURE_STATUS_CODE = 1
 
@@ -11,14 +10,12 @@ def send_data(server_ip: str, server_port: int, data: str) -> None:
     """
     Send data to server in address (server_ip, server_port).
     """
-    sock = socket.create_connection((server_ip, server_port))
-    data_bytes = data.encode("utf-8")
-    print("sending message...")
-    sock.sendall(struct.pack("<I", len(data_bytes)))
-    sock.sendall(data_bytes)
+    with Connection.connect(server_ip, server_port) as to_send_connection:
+        print("sending message...")
+        to_send_connection.send_message(data.encode("utf-8"))
 
 
-def get_args():
+def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Send data to server.")
     parser.add_argument("server_ip", type=str, help="the server's ip")
     parser.add_argument("server_port", type=int, help="the server's port")
@@ -26,7 +23,7 @@ def get_args():
     return parser.parse_args()
 
 
-def main():
+def main() -> int:
     """
     Implementation of CLI and sending data to server.
     """
@@ -34,6 +31,7 @@ def main():
     try:
         send_data(args.server_ip, args.server_port, args.data)
         print("Done.")
+        return 0
     except Exception as error:
         print(f"ERROR: {error}")
         return FAILURE_STATUS_CODE
